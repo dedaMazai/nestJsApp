35b266464e30cd9e37fb05eb63436a081be57b34
Andrei Chipizubov <andryukha.chipizybov@mail.ru>
2024-07-01 11:14:57 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,24 @@
-import {NestFactory} from "@nestjs/core";
-import {AppModule} from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import {JwtAuthGuard} from "./auth/jwt-auth.guard";
 import {ValidationPipe} from "./pipes/validation.pipe";
 
+async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
 
-async function start() {
-    const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+  .setTitle('Урок по продвинотому BACKEND')
+  .setDescription('Документация REST API')
+  .setVersion('1.0.0')
+  .addTag('Tag')
+  .build()
 
-    const config = new DocumentBuilder()
-        .setTitle('Урок по продвинотому BACKEND')
-        .setDescription('Документация REST API')
-        .setVersion('1.0.0')
-        .addTag('Tag')
-        .build()
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('/api/docs', app, document)
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document)
+  app.useGlobalPipes(new ValidationPipe())
 
-    app.useGlobalPipes(new ValidationPipe())
-
-    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
+bootstrap();
 
-start()
